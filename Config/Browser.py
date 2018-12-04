606a332411600bef88b4a9d10895dbe713047bcd
AndreyMarunyak <and.marunyak@gmail.com>
2018-12-04 23:22:49 +0200

    add try block to cookies loading

--- Config/Browser.py
+++ Config/Browser.py
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import pickle
 
+from Config.Data import DRIVERPATH
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-from Config import Data, FindElement
-
-
 
 def chrome():
 
@@ -15,7 +10,7 @@ def chrome():
     prefs = {'profile.default_content_setting_values.notifications': 2}
     chrome_options.add_experimental_option("prefs", prefs)
 
-    return webdriver.Chrome(executable_path=Data.DRIVERPATH, chrome_options=chrome_options)
+    return webdriver.Chrome(executable_path=DRIVERPATH, chrome_options=chrome_options)
 
 def saveCookies(browser):
     pickle.dump(browser.get_cookies(), open('cookies.pkl', 'wb'))
@@ -25,14 +20,3 @@ def loadCookies(browser):
     for cookie in cookies:
         browser.add_cookie(cookie)
 
-=======
-from selenium import webdriver
-from Config import Data
-
-
-class Browser():
-
-    @staticmethod
-    def chrome():
-        return webdriver.Chrome(executable_path=Data.DRIVERPATH)
->>>>>>> f31d7f2... add config files and refactor
--- FacebookTests/SuccesLogin.py
+++ FacebookTests/SuccesLogin.py
@@ -1,4 +1,3 @@
-
 from Config.Data import *
 from Config.Browser import *
 
@@ -12,23 +11,11 @@ POST_TEXT = 'Принёс жертву богу опп'
 
 chrome = chrome()
 chrome.implicitly_wait(30)
-from Config import Data
-from Config import Browser
-
-import unittest
-
-URL = 'http://facebook.com'
-
-LOGIN = Data.LOGIN
-PASSWORD = Data.PASSWORD
-DRIVERPATH = Data.DRIVERPATH
-
-chrome = Browser.Browser.chrome()
-
 
 class SuccessLoginTest(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         chrome.get(URL)
 
     def testLogin(self):
@@ -42,32 +29,18 @@ class SuccessLoginTest(unittest.TestCase):
 
         assert chrome.title == 'Facebook', 'Wrong title'
 
-    def testOpenBrowser(self):
-
-        chrome.find_element_by_id('email').send_keys(LOGIN)
-        chrome.find_element_by_id('pass').send_keys(PASSWORD)
-        chrome.find_element_by_id('loginbutton').click()
-        print(chrome.title)
-        assert chrome.title == 'Facebook'
-
-    def tearDown(self):
-        chrome.close()
-        chrome.quit()
-
-
-class SuccessPostAdding(unittest.TestCase):
-
-    def setUp(self):
-        loadCookies(chrome)
-        chrome.get(URL)
-        # chrome.refresh()
-
     def testPostAdding(self):
 
-
+        if chrome.title != 'Facebook':
+            chrome.get(URL)
+            try:
+                loadCookies(chrome)
+                chrome.refresh()
+            except Exception as e:
+                print(e, ': no cookies file')
 
         chrome.find_element(By.XPATH, SEARCH_FIELD).send_keys('QA School 2018 - Kherson')
-        chrome.send_keys(Keys.ENTER)
+        chrome.find_element(By.XPATH, SEARCH_FIELD).send_keys(Keys.ENTER)
         chrome.find_element(By.XPATH, QA_SCHOOL_LINK).click()
         chrome.find_element(By.XPATH, POST_FIELD).send_keys(POST_TEXT)
         chrome.find_element(By.XPATH, SEND_POST_BUTTON).click()
@@ -76,6 +49,11 @@ class SuccessPostAdding(unittest.TestCase):
         time.sleep(5)
         chrome.find_element(By.XPATH, LIKE_BUTTON).click()
         assert (POST_TEXT in chrome.page_source)
+
+    @classmethod
+    def tearDownClass(cls):
+        chrome.close()
+        chrome.quit()
 
 
 if __name__ == '__main__':
